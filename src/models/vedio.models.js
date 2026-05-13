d859742e751b81e3dac154b44f948af3096829cb
aj-1729 <ashwinijhadps@gmail.com>
2026-05-13 19:57:20 +0530

    added user and vedio model

---
+++ src/models/vedio.models.js
@@ -0,0 +1,43 @@
+import mongoos, {Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const vedioSchema = new Schema({
+    vedioFile:{
+        type: String,
+        required: true,     
+    },
+    thumbnail:{
+        type: String,
+        required: true,
+    },
+    title:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    duration:{
+        type: Number,
+        required: true,
+    },
+    views:{
+        type: Number,
+        default: 0,
+    },
+    isPublished:{
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: Schema.Types.objectId,
+        ref: "User"
+    }
+},{
+    timestamps: true
+})
+
+
+vedioSchema.plugin(mongooseAggregatePaginate)
+
+export const Vedio = mongoos.model("Vedio", vedioSchema);
